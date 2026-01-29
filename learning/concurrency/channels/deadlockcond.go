62d6904d6e2fcdb34b9b437db7fffb51faea5c4b
dhirendraj-cmd <djha5051@gmail.com>
2026-01-30 00:38:06 +0530

    go channels

---
+++ learning/concurrency/channels/deadlockcond.go
@@ -0,0 +1,17 @@
+package channels
+
+import "fmt"
+
+func DeadLockChan(){
+	msgChan := make(chan string)
+
+	msgChan <- "Learning GoLang" // blocking
+
+	msgVal := <- msgChan
+
+	fmt.Println(msgVal)
+}
+
+/*
+	msgchan here is a channel sending string value to msgVal, but channels are blocking so deadlock condition arise which is solved in messages.go
+*/
